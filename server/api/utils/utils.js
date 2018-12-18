e7933e3036dadbc92123f95e8249bc62f7a38d4e
tzweb <821189067@qq.com>
2018-12-18 17:23:45 +0800

    update

--- server/api/utils/utils.js
+++ server/api/utils/utils.js
@@ -1,17 +1,1 @@
-// 处理图片路径
-const baseUrl = 'http://127.0.0.1:3000/images'
-const utils = {
-  preUrl:(obj) => {
-    if (!obj) {
-      return
-    }
-    for (let value of obj) {
-      if (value && value.Image) {
-        value.Image.url = baseUrl + value.Image.url
-      }
-    }
-    return obj
-  }
-}
-
-export default utils
+// utils
